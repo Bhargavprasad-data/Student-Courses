a953c34f15c12e493129c3f78fb5866f35772a14
Bhargavprasad Vana <23331a4462@mvgrce.edu.in>
2025-10-28 19:08:13 +0530

    First commit

---
+++ funct.js
@@ -0,0 +1,7 @@
+function Home(a,b){
+    if(a === "yourgmail@gmail.com" && b === "Bhargav@123"){
+        window.location.href="Home.html";
+    } else {
+        window.location.href="Home.html";
+    }
+}
